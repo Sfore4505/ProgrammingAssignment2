94e366d4b38762ab17b4e7e9c88c1852b1c558eb
Sfore4505 <sharayahrusso@gmail.com>
2014-12-17 08:08:19 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##function is storing a value or "caching" it in order to save time with computation
+##when being recalled
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+  x<<-y 
+  m<<-NULL
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#<<- assigns variable to an environment that is different than the current one
+get<-function() x
+setmatrix<-function(solve) m<<- solve #getting inverse using solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
 }
+#first two functions set up matrix to get values
+#reference: http://stackoverflow.com/questions/11995832/inverse-of-matrix-in-r
+#reference: http://www.ats.ucla.edu/stat/r/library/matrix_alg.htm
+
+cacheSolve <- function(x=matrix(), ...) {
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("Getting cached data!")
+      return(m)
+    }
+    matrix <- x$get() 
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
+}
+#actual cashe function, recalling the value
